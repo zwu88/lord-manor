62e6f40e99aa8f274781010859d7de4d8ce00048
Zhikai Wu <134594806+zwu88@users.noreply.github.com>
2026-06-26 01:08:40 +0800

    Create departments.js

---
+++ functions/_lib/departments.js
@@ -0,0 +1,16 @@
+export const DEPARTMENT_IDS = Object.freeze([
+  "research-institute",
+  "academy",
+  "health-commission",
+  "household-affairs",
+  "music-department",
+  "external-relations",
+  "travel-department",
+  "council-chamber",
+  "chronicle-department",
+  "treasury-office"
+]);
+
+export const VALID_DEPARTMENTS = new Set(
+  DEPARTMENT_IDS
+);
